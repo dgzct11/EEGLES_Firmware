d1df3b425a3265b4bddb8673e1c6eb2c523138fc
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2023-05-01 13:51:06 -0500

    dd

--- TalonFirmware/main.c
+++ TalonFirmware/main.c
@@ -4,9 +4,7 @@
 #include "pico/multicore.h"
 #include "hardware/i2c.h"
 #include "hardware/spi.h"
-#include "hardware/pio.h"
-#include "hardware/uart.h"
-
+#include "hardware/pio.h" #include "hardware/uart.h"
 
 #include <stdint.h>
 
@@ -158,7 +156,7 @@ ADS1299
 #define ADS1299_CMD_RDATA      0x12
 
 #define ADS1299_VREF 4.5
-#define ADS1299_GAIN 1
+#define ADS1299_GAIN 24
 
 #define READ_BIT 0x20
 #define WRITE_BIT 0x40
@@ -302,40 +300,40 @@ void ADS1299_init() {
     //send SDATAC command to write the registers
     ADS1299_cs_select();
     ADS1299_send_command(ADS1299_CMD_SDATAC);
-    ADS1299_write_register(ADS1299_REG_CONFIG3, 0b11000000);
+    ADS1299_write_register(ADS1299_REG_CONFIG3, 0xe0);
     sleep_ms(1000);
 
     //write configuration registers
 
         //enable CLK output and Set output data rate to 250SPS
-        ADS1299_write_register(ADS1299_REG_CONFIG1,  0b10000100);
+        ADS1299_write_register(ADS1299_REG_CONFIG1,  0b11010011);
 
-        ADS1299_write_register(ADS1299_REG_CONFIG2,  0b00000000);
+        //testing settings
+        ADS1299_write_register(ADS1299_REG_CONFIG2,  0b11010000);
 
 
         //Bias Settings
-        ADS1299_write_register(ADS1299_REG_CONFIG3,  0b11111111);
+        ADS1299_write_register(ADS1299_REG_CONFIG3,  0b11101110);
         
         //close SRB1 swithces
         ADS1299_write_register(ADS1299_REG_MISC1, 0b00100000);
 
         //close BIASP switches
-        ADS1299_write_register(ADS1299_REG_BIAS_SENSP, 0b11111111);
+        ADS1299_write_register(ADS1299_REG_BIAS_SENSP, 0b00000000);
 
         //set channel to normal electrode input
-        ADS1299_write_register(ADS1299_REG_CH1SET, 0b00000000);
-        ADS1299_write_register(ADS1299_REG_CH2SET, 0b00000000);
-        ADS1299_write_register(ADS1299_REG_CH3SET, 0b00000000);
-        ADS1299_write_register(ADS1299_REG_CH4SET, 0b00000000);
-        ADS1299_write_register(ADS1299_REG_CH5SET, 0b00000000);
-        ADS1299_write_register(ADS1299_REG_CH6SET, 0b00000000);
+        ADS1299_write_register(ADS1299_REG_CH1SET, 0b01100000);
+        ADS1299_write_register(ADS1299_REG_CH2SET, 0b01100000);
+        ADS1299_write_register(ADS1299_REG_CH3SET, 0b01100000);
+        ADS1299_write_register(ADS1299_REG_CH4SET, 0b01100000);
+        ADS1299_write_register(ADS1299_REG_CH5SET, 0b01100000);
+        ADS1299_write_register(ADS1299_REG_CH6SET, 0b01100000);
 
 
-        //lead off detection
-
-            ADS1299_write_register(ADS1299_REG_LOFF, 0x13);
+            ADS1299_write_register(ADS1299_REG_LOFF, 0b00001100);
             ADS1299_write_register(ADS1299_REG_CONFIG4, 0x02 );
             ADS1299_write_register(ADS1299_REG_LOFF_SENSP, 0xFF);
+            ADS1299_write_register(ADS1299_REG_LOFF_SENSN, 0x00);
     
     
     //initialize start pin
--- graph.py
+++ graph.py
@@ -48,20 +48,26 @@ class Graph:
 
     def get_data(self):
         try:
-            data = list(map(float, self.ser.readline().decode().split(' ')[:-1]))
-            print(f'{data[-3]:08b}'[4:] + f'{data[-2]:08b}'[:2])
+            
+            data = list(map(float, self.ser.readline().decode().rstrip().rstrip().split(' ')))
+            print(data)
+            #print(f'{int(data[-3]):08b}'[4:] + f'{int(data[-2]):08b}'[:2])
             data = data[:-3]
             if len(data) == self.eeg_channels:
                 self.buffer.append(data)
             if len(self.buffer) > self.sampling_rate * self.window:
                 self.buffer = self.buffer[(-self.sampling_rate * self.window):]
+                
         except ValueError:
+            print("error")
             pass
+            
 
         return np.asarray(self.buffer).T
 
     def update(self):
         data = self.get_data()
+        
 
         for channel in range(self.eeg_channels):
             # DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
@@ -74,5 +80,6 @@ class Graph:
             # DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
             #                             FilterTypes.BUTTERWORTH.value, 0)
             self.curves[channel].setData(data[channel].tolist())
+            
 
         self.app.processEvents()
